cfbce8858cd1c144a17b72c57740ade65be9a27a
jgnacio <jgnaciogomez406@gmail.com>
2023-04-11 10:52:59 -0700

    Add stats function to app_views

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 """This module provides a simple get status of the web server."""
 from api.v1.views import app_views
+from models import storage
+from console import classes
+
 
 @app_views.route("/status", strict_slashes=False)
 def status():
     """Return OK status."""
     return {"status": "OK"}
+
+
+@app_views.route("/stats", strict_slashes=False)
+def stats():
+    """Get the number of instances of each class."""
+    return {cls_key: storage.count(cls_obj) for cls_key,
+            cls_obj in classes.items()}
